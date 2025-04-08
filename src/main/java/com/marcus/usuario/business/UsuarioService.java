46942d2f883a641ab8e04900d4d4734fdee701ed
Marcus Vinicius <marcusmendes746@gmail.com>
2025-04-08 13:10:32 -0300

    inclusao de metodo put de usuario.

--- src/main/java/com/marcus/usuario/business/UsuarioService.java
+++ src/main/java/com/marcus/usuario/business/UsuarioService.java
@@ -1,5 +1,6 @@
 package com.marcus.usuario.business;
 
+import org.springframework.boot.autoconfigure.security.oauth2.resource.OAuth2ResourceServerProperties.Jwt;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -9,24 +10,27 @@ import com.marcus.usuario.infrastructure.entity.Usuario;
 import com.marcus.usuario.infrastructure.exceptions.ConflictException;
 import com.marcus.usuario.infrastructure.exceptions.ResourceNotFoundException;
 import com.marcus.usuario.infrastructure.repository.UsuarioRepository;
+import com.marcus.usuario.infrastructure.security.JwtUtil;
+
 import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
 public class UsuarioService {
-    
+
     private final UsuarioRepository usuarioRepository;
     private final UsuarioConverter usuarioConverter;
     private final PasswordEncoder PasswordEncoder;
+    private final JwtUtil jwtUtil;
 
-    public UsuarioDTO salvaUsuario(UsuarioDTO usuarioDTO){
+    public UsuarioDTO salvaUsuario(UsuarioDTO usuarioDTO) {
         emailExiste(usuarioDTO.getEmail());
         usuarioDTO.setSenha(PasswordEncoder.encode(usuarioDTO.getSenha()));
         Usuario usuario = usuarioConverter.paraUsuario(usuarioDTO);
         return usuarioConverter.paraUsuarioDTO(usuarioRepository.save(usuario));
     }
 
-      public void emailExiste(String email) {
+    public void emailExiste(String email) {
         try {
             boolean existe = verificaEmailExistente(email);
             if (existe) {
@@ -41,13 +45,25 @@ public class UsuarioService {
         return usuarioRepository.existsByEmail(email);
     }
 
-     public Usuario buscaUsuarioPorEmail(String email) {
+    public Usuario buscaUsuarioPorEmail(String email) {
         return usuarioRepository.findByEmail(email)
                 .orElseThrow(() -> new ResourceNotFoundException("email não encontrado" + email));
     }
 
     public void deletaUsuarioPorEmail(String email) {
         usuarioRepository.deleteByEmail(email);
-     }
+    }
+
+    public UsuarioDTO atualizaDadosUsuario(String token, UsuarioDTO dto) {
+        String email = jwtUtil.extraiEmailToken(token.substring(7));
+
+        dto.setSenha(dto.getSenha() != null ? PasswordEncoder.encode(dto.getSenha()) : null);
+        Usuario usuarioEntity = usuarioRepository.findByEmail(email)
+                .orElseThrow(() -> new ResourceNotFoundException("email não encontrado" + email));
+
+        Usuario usuario = usuarioConverter.updateUsuario(dto, usuarioEntity);
+
+        return usuarioConverter.paraUsuarioDTO(usuarioRepository.save(usuario));
+    }
 
 }
--- src/main/java/com/marcus/usuario/business/converter/UsuarioConverter.java
+++ src/main/java/com/marcus/usuario/business/converter/UsuarioConverter.java
@@ -15,18 +15,15 @@ import com.marcus.usuario.infrastructure.entity.Usuario;
 @Component
 public class UsuarioConverter {
 
-    public Usuario paraUsuario(UsuarioDTO usuarioDTO){
+    public Usuario paraUsuario(UsuarioDTO usuarioDTO) {
         return Usuario.builder()
-            .nome(usuarioDTO.getNome())
-            .email(usuarioDTO.getEmail())
-            .senha(usuarioDTO.getSenha())
-            .enderecos(paraListaEndereco(usuarioDTO.getEnderecos()))
-            .telefones(paraListaTelefonesDTO(usuarioDTO.getTelefones()))
+                .nome(usuarioDTO.getNome())
+                .email(usuarioDTO.getEmail())
+                .senha(usuarioDTO.getSenha())
+                .enderecos(paraListaEndereco(usuarioDTO.getEnderecos()))
+                .telefones(paraListaTelefonesDTO(usuarioDTO.getTelefones()))
 
-        
-        
-        
-        .build();
+                .build();
     }
 
     public List<Endereco> paraListaEndereco(List<EnderecoDTO> enderecoDto) {
@@ -35,37 +32,38 @@ public class UsuarioConverter {
                 .collect(Collectors.toList());
     }
 
-    public Endereco paraEndereco(EnderecoDTO enderecoDTO){
+    public Endereco paraEndereco(EnderecoDTO enderecoDTO) {
         return Endereco.builder()
-            .rua(enderecoDTO.getRua())
-            .numero(enderecoDTO.getNumero())
-            .cidade(enderecoDTO.getCidade())
-            .estado(enderecoDTO.getEstado())
-            .cep(enderecoDTO.getCep())
-            .complemento(enderecoDTO.getComplemento())
-        
-        .build();
+                .rua(enderecoDTO.getRua())
+                .numero(enderecoDTO.getNumero())
+                .cidade(enderecoDTO.getCidade())
+                .estado(enderecoDTO.getEstado())
+                .cep(enderecoDTO.getCep())
+                .complemento(enderecoDTO.getComplemento())
+
+                .build();
     }
 
-    public List<Telefone> paraListaTelefonesDTO(List<TelefoneDTO> telefoneDTO){
-            return telefoneDTO.stream().map(this::paraTelefone).toList();
+    public List<Telefone> paraListaTelefonesDTO(List<TelefoneDTO> telefoneDTO) {
+        return telefoneDTO.stream().map(this::paraTelefone).toList();
     }
-    public Telefone paraTelefone(TelefoneDTO telefoneDTO){
+
+    public Telefone paraTelefone(TelefoneDTO telefoneDTO) {
         return Telefone.builder()
-            .ddd(telefoneDTO.getDdd())
-            .numero(telefoneDTO.getNumero())
-        
-        .build();
+                .ddd(telefoneDTO.getDdd())
+                .numero(telefoneDTO.getNumero())
+
+                .build();
     }
 
-    public UsuarioDTO paraUsuarioDTO(Usuario usuarioDTO){
+    public UsuarioDTO paraUsuarioDTO(Usuario usuarioDTO) {
         return UsuarioDTO.builder()
-            .nome(usuarioDTO.getNome())
-            .email(usuarioDTO.getEmail())
-            .senha(usuarioDTO.getSenha())
-            .enderecos(paraListaEnderecoDTO(usuarioDTO.getEnderecos()))
-            .telefones(paraListaTelefonesdto(usuarioDTO.getTelefones()))
-        .build();
+                .nome(usuarioDTO.getNome())
+                .email(usuarioDTO.getEmail())
+                .senha(usuarioDTO.getSenha())
+                .enderecos(paraListaEnderecoDTO(usuarioDTO.getEnderecos()))
+                .telefones(paraListaTelefonesdto(usuarioDTO.getTelefones()))
+                .build();
     }
 
     public List<EnderecoDTO> paraListaEnderecoDTO(List<Endereco> enderecoDto) {
@@ -74,26 +72,38 @@ public class UsuarioConverter {
                 .collect(Collectors.toList());
     }
 
-    public EnderecoDTO paraEnderecoDTO(Endereco enderecoDTO){
+    public EnderecoDTO paraEnderecoDTO(Endereco enderecoDTO) {
         return EnderecoDTO.builder()
-            .rua(enderecoDTO.getRua())
-            .numero(enderecoDTO.getNumero())
-            .cidade(enderecoDTO.getCidade())
-            .estado(enderecoDTO.getEstado())
-            .cep(enderecoDTO.getCep())
-            .complemento(enderecoDTO.getComplemento())
-        
-        .build();
+                .rua(enderecoDTO.getRua())
+                .numero(enderecoDTO.getNumero())
+                .cidade(enderecoDTO.getCidade())
+                .estado(enderecoDTO.getEstado())
+                .cep(enderecoDTO.getCep())
+                .complemento(enderecoDTO.getComplemento())
+
+                .build();
     }
 
-    public List<TelefoneDTO> paraListaTelefonesdto(List<Telefone> telefoneDTO){
-            return telefoneDTO.stream().map(this::paraTelefone).toList();
+    public List<TelefoneDTO> paraListaTelefonesdto(List<Telefone> telefoneDTO) {
+        return telefoneDTO.stream().map(this::paraTelefone).toList();
     }
-    public TelefoneDTO paraTelefone(Telefone telefoneDTO){
+
+    public TelefoneDTO paraTelefone(Telefone telefoneDTO) {
         return TelefoneDTO.builder()
-            .ddd(telefoneDTO.getDdd())
-            .numero(telefoneDTO.getNumero())
-        
-        .build();
+                .ddd(telefoneDTO.getDdd())
+                .numero(telefoneDTO.getNumero())
+
+                .build();
+    }
+
+    public Usuario updateUsuario(UsuarioDTO usuarioDTO, Usuario entity) {
+        return Usuario.builder()
+                .nome(usuarioDTO.getNome() != null ? usuarioDTO.getNome() : entity.getNome())
+                .id(entity.getId())
+                .senha(usuarioDTO.getSenha() != null ? usuarioDTO.getSenha() : entity.getSenha())
+                .enderecos(entity.getEnderecos())
+                .telefones(entity.getTelefones())
+                .email(usuarioDTO.getEmail() != null ? usuarioDTO.getEmail() : entity.getEmail())
+                .build();
     }
 }
